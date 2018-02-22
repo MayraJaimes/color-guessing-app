2336fc4315b5433930010a2bdaa5065f75438e01
Mayra Jaimes <mtj26@georgetown.edu>
2018-02-22 16:58:43 -0600

    giving squares different colors and setting the color user needs to guess

--- colorgame.js
+++ colorgame.js
@@ -1,7 +1,24 @@
-var colors = [];
+var colors = [
+    "pink", "blue", "red", "orange", "black", "white"
+];
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.querySelector("#colorDisplay");
 
 for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];  
+    squares[i].addEventListener("click", function(){
+        var clickedColor = this.style.background;
+
+        if (clickedColor === pickedColor){
+            //correct
+        }
+        else {
+            //incorrect
+        }
+    }) 
+
 }
+
+colorDisplay.textContent = pickedColor;
